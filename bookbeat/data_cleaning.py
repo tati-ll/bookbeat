76967e807fc3bede07975043f113ec5c0253ec8c
tatiana llana <tjllana@uc.cl>
2024-01-02 12:50:32 -0300

    Se crea función para limpieza de texto (específico para letras de canciones) en archivo data_cleaning.py

--- bookbeat/data_cleaning.py
+++ bookbeat/data_cleaning.py
@@ -43,3 +43,45 @@ def cleaning_books(sentence):
     sentence = re.sub(r'\s+', ' ', sentence).strip()
 
     return sentence
+
+
+def cleaning_songs(sentence, language):
+    """Función que realiza limpieza de texto, específica para letras de canciones.
+    Realiza limpieza básica (quitar espacios al inicio/final, puntuación, símbolos, números, pasar a minúsculas)
+    Limpieza avanzada: Tokenizar y eliminar stopwords (en 2 idiomas: en, es).
+    Quita símbolos y palabras específicas usando Re"""
+
+    sentence = sentence.strip()
+    sentence = sentence.lower()
+    sentence = ''.join(char for char in sentence if not char.isdigit())
+
+    for punctuation in string.punctuation:
+        sentence = sentence.replace(punctuation, '')
+
+    # Eliminar palabras específicas
+    words_to_remove = ['uoh', 'yeah', 'ta', 'pumpumpum', 'tumtumtum', 'brumbrumbrum', 'pal', 'pa', 'ay', 'ey', 'wouwouwou',
+                       'ihhhyo', 'ihhhye', 'shalala', 'lalala', 'nanana', 'na', 'uh', 'ah', 'ohh', 'oh', 'ooh', 'ho',
+                       'mmm', 'hey', 'im', 'oh', 'ohhh', 'ohohoh', 'em', 'ne', 'yo', 'en', 'aint', 'lay', 'youre', 'gon',
+                       'til', 'nana', 'wan']
+    for word in words_to_remove:
+        sentence = re.sub(r'\b' + re.escape(word) + r'\b', '', sentence)
+
+    # Eliminar símbolos específicos que no están dentro de string.punctuation
+    sentence = re.sub(r'[”“—’…‘¡¿]', '', sentence)
+
+    # Tokenizar
+    tokenized_sentence = word_tokenize(sentence)
+
+    # Define las stopwords según el idioma
+    if language == 'en':
+        stop_words = set(stopwords.words('english'))
+    elif language == 'es':
+        stop_words = set(stopwords.words('spanish'))
+    else:
+        stop_words = set()  # Si el idioma no está en la lista, no eliminar stopwords
+
+    tokenized_sentence_cleaned = [w for w in tokenized_sentence if not w in stop_words]
+
+    cleaned_sentence = ' '.join(word for word in tokenized_sentence_cleaned)
+
+    return cleaned_sentence
