70ff5cd15810814f7cec5d7370158ae22b4f3bff
Cecilia Gatta <cgatta13@gmail.com>
2024-01-06 16:05:36 -0300

    interface usario version 1

--- bookbeat-website/app.py
+++ bookbeat-website/app.py
@@ -4,6 +4,11 @@ import os
 import numpy as np
 import pandas as pd
 
+import sys
+sys.path.append("..")
+
+from bookbeat.playlist_generation import playlist_popularity
+
 '''
 # BOOKBEAT'''
 
@@ -12,11 +17,34 @@ st.markdown("""
 """)
 
 # Ruta al archivo CSV
-ruta_archivo = "/home/cecigatta/code/ceci-jitsi/bookbeat/raw_data/books_sentiment.csv"
+ruta_books = "../raw_data/books_sentiment.csv"
+ruta_songs = "../raw_data/songs_sentiment.csv"
 
 # Cargar datos desde el archivo CSV
-data = pd.read_csv(ruta_archivo)
+data_book = pd.read_csv(ruta_books)
+data_songs = pd.read_csv(ruta_songs)
+
 
 # Crear un menú desplegable con la lista de libros
-libro_seleccionado = st.selectbox("Selecciona un libro:", data['Book'].tolist(), index=0, key='libro')
-libros_seleccionados = st.multiselect("Selecciona un libro:", data['Book'].tolist(), key="libro")
+#libro_seleccionado = st.selectbox("Selecciona un libro:", data['Book'].tolist(), index=0, key='libro')
+title = st.multiselect("Selecciona un libro:", data_book['Book'].tolist(), key="libro")
+
+#Selección de géneros musicales
+# genero = ['pop', 'rock', 'r&b', 'rap', 'edm', 'latin']
+#tipo_musica = st.multiselect("Selecciona generos:", genero, key="musica")
+
+if title:
+    st.write(f"Libro seleccionado: {title[0]}")
+else:
+    st.warning("No se seleccionó ningún libro.")
+
+# Llamar a la función de generación de playlist
+if st.button("Generar Playlist"):
+    # Resultados de la función basada en el título del libro
+    libro = title[0]
+    resultados_playlist = playlist_popularity(libro,data_book, data_songs)
+
+    uri= resultados_playlist['URI'].tolist()
+
+    # Mostrar resultados en Streamlit
+    st.write(resultados_playlist[['track_name','track_artist','URI']])
