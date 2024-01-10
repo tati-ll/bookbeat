fcd9bc486c7efbfaaa01d1d3b0df53531326d2db
Cecilia Gatta <cgatta13@gmail.com>
2024-01-09 23:05:24 -0300

    Modificaciones estilo

--- bookbeat-website/app2.py
+++ bookbeat-website/app2.py
@@ -9,6 +9,12 @@ sys.path.append("..")
 
 from bookbeat.playlist_generation import playlist_popularity
 
+# -- Set page config
+apptitle = 'BookBeat'
+
+st.set_page_config(page_title=apptitle, page_icon=":book:")
+
+
 @st.cache_data
 def carga_dataset():
     # Ruta al archivo CSV
@@ -31,29 +37,29 @@ with col2:
    st.image("../raw_data/bookbeat.jpg")
 
 
-st.markdown("""
-**Selecciona el título de tu libro**
-""")
+#st.markdown("""
+#**Selecciona el título de tu libro**
+#""")
 
-data_book, data_songs = carga_dataset()
+# data_book, data_songs = carga_dataset()
 
-# Crear un menú desplegable con la lista de libros
-title = st.selectbox("Selecciona el título de tu libro:", data_book['Book'].tolist(), index=None, key='libro1', placeholder="Choose an option", label_visibility='hidden')
+# # Crear un menú desplegable con la lista de libros
+# title = st.selectbox("Selecciona el título de tu libro:", data_book['Book'].tolist(), index=None, key='libro1', placeholder="Choose an option", label_visibility='hidden')
 
-#Selección de géneros musicales
-# genero = ['pop', 'rock', 'r&b', 'rap', 'edm', 'latin']
-#tipo_musica = st.multiselect("Selecciona generos:", genero, key="musica")
+# #Selección de géneros musicales
+# # genero = ['pop', 'rock', 'r&b', 'rap', 'edm', 'latin']
+# #tipo_musica = st.multiselect("Selecciona generos:", genero, key="musica")
 
-if not title:
-    st.warning("No se seleccionó ningún libro.")
+# if not title:
+#     st.warning("No se seleccionó ningún libro.")
 
-# Llamar a la función de generación de playlist
-if st.button("Generar Playlist"):
-    # Resultados de la función basada en el título del libro
-    libro = title
-    resultados_playlist = playlist_popularity(libro, data_book, data_songs)
+# # Llamar a la función de generación de playlist
+# if st.button("Generar Playlist"):
+#     # Resultados de la función basada en el título del libro
+#     libro = title
+#     resultados_playlist = playlist_popularity(libro, data_book, data_songs)
 
-    uri= resultados_playlist['URI'].tolist()
+#     uri= resultados_playlist['URI'].tolist()
 
-    # Mostrar resultados en Streamlit
-    st.write(resultados_playlist[['track_name','track_artist','URI']])
+#     # Mostrar resultados en Streamlit
+#     st.write(resultados_playlist[['track_name','track_artist','URI']])
