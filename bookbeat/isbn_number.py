c704912eff0db0c14dfd4b0b6a7dfece814a79b4
Cecilia Gatta <cgatta13@gmail.com>
2024-01-11 22:39:29 -0300

    avance api google books

--- bookbeat/isbn_number.py
+++ bookbeat/isbn_number.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import numpy as np
 
-def book_isbn(titulo_libro: str, dataframe):
+def book_isbn(titulo_libro: str, autor: str, dataframe):
     '''Funcion para obtener el numero de ISBN a partir de un título'''
     # Convertir el título del libro a minúsculas para realizar una búsqueda sin distinción entre mayúsculas y minúsculas
     titulo_libro = titulo_libro.lower()
+    autor_libro = autor.lower()
 
     # Filtrar el DataFrame para encontrar el ISBN correspondiente al título
-    resultado = dataframe[dataframe['Title'].str.lower() == titulo_libro]
+    resultado = dataframe[(dataframe['Title'].str.lower() == titulo_libro) &
+                          (dataframe['Author'].str.lower() == autor_libro)]
 
     if not resultado.empty:
         # Si se encuentra una coincidencia, devolver el ISBN
